a8bfb01d34dd5ac8d9c3323c56283b46add8eb5a
dhaval dhanani <dhaval070@gmail.com>
2021-06-22 23:23:50 +0530

    updated data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 import MyChart from './MyChart';
-import rawData from './rawdata.js';
+import data from './rawdata.js';
 
 function App() {
     let map = {};
 
-    for (let row of rawData) {
+    for (let row of data) {
         const dt = new Date(row.period);
+        console.log(dt);
         const sdate = (dt.getMonth() + 1) + '/' + dt.getDate() + '/' + dt.getFullYear();
 
         if (!map[sdate]) map[sdate] = { period: dt };
-        map[sdate][row.sales_channel] = row.sales;
+        map[sdate][row.channel] = row.sales;
     }
 
-    console.log(map);
+    let values = Object.values(map).sort((a, b) => {
+        return a.period - b.period;
+    });
+
   return (
     <div className="App">
         <h3>Chart Test Demo (Dhaval Dhanani)</h3> <br />
         <h4>Sales Forecast</h4>
-        <MyChart data={Object.values(map)}></MyChart>
+        <MyChart data={values}></MyChart>
     </div>
   );
 }
